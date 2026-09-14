2ad70701473f63451c07d2e0d79e0ec882562cda
Ben Letchford <ben@letchford.cloud>
2026-09-15 00:24:41 +1000

    refactor(ppc): pre-resolve AppleTalk operations

--- src/loader/ppc/tests/hardware_compatibility.rs
+++ src/loader/ppc/tests/hardware_compatibility.rs
@@ -1,6 +1,43 @@
 use super::*;
 
 #[test]
+fn appletalk_compatibility_imports_pre_resolve_to_typed_operations() {
+    for (symbol, operation) in [
+        (
+            "GetBridgeAddress",
+            PpcAppleTalkCompatibilityOperation::GetBridgeAddress,
+        ),
+        (
+            "GetNodeAddress",
+            PpcAppleTalkCompatibilityOperation::GetNodeAddress,
+        ),
+        ("GetZoneList", PpcAppleTalkCompatibilityOperation::GetZoneList),
+        ("MPPOpen", PpcAppleTalkCompatibilityOperation::MppOpen),
+        ("NBPExtract", PpcAppleTalkCompatibilityOperation::NbpExtract),
+        ("NBPSetEntity", PpcAppleTalkCompatibilityOperation::NbpSetEntity),
+        ("NBPSetNTE", PpcAppleTalkCompatibilityOperation::NbpSetNte),
+        ("PCloseSkt", PpcAppleTalkCompatibilityOperation::PCloseSkt),
+        ("PKillNBP", PpcAppleTalkCompatibilityOperation::PKillNbp),
+        ("PLookupName", PpcAppleTalkCompatibilityOperation::PLookupName),
+        ("POpenSkt", PpcAppleTalkCompatibilityOperation::POpenSkt),
+        (
+            "PRegisterName",
+            PpcAppleTalkCompatibilityOperation::PRegisterName,
+        ),
+        ("PRemoveName", PpcAppleTalkCompatibilityOperation::PRemoveName),
+        ("PSetSelfSend", PpcAppleTalkCompatibilityOperation::PSetSelfSend),
+        ("PWriteDDP", PpcAppleTalkCompatibilityOperation::PWriteDdp),
+        ("StandardNBP", PpcAppleTalkCompatibilityOperation::StandardNbp),
+    ] {
+        assert_eq!(
+            dispatcher_target_for_import("InterfaceLib", symbol),
+            PpcImportDispatcherTarget::AppleTalkCompatibility(operation),
+            "unexpected AppleTalk dispatch target for {symbol}",
+        );
+    }
+}
+
+#[test]
 fn native_ppc_munger_replaces_live_handle_contents() {
     let mut memory = PpcSectionMem::new();
     memory.add_region(0x1000, vec![0; 4]);
@@ -48,15 +85,49 @@ fn native_ppc_missing_classic_hardware_reports_canonical_errors() {
     let mut cpu = PpcCpu::new();
     cpu.gpr[3] = 0x1000;
     cpu.gpr[4] = 0x1004;
-    let binding = compatibility_binding(
-        "InterfaceLib",
-        "GetNodeAddress",
-        PpcImportDispatcherTarget::AppleTalkCompatibility,
-    );
     assert_eq!(
-        ppc_dispatch_appletalk_compatibility(&binding, &mut cpu, &mut memory),
+        ppc_dispatch_appletalk_compatibility(
+            PpcAppleTalkCompatibilityOperation::GetNodeAddress,
+            &mut cpu,
+            &mut memory,
+        ),
         PpcImportAction::Return(ppc_i16_result(PPC_NO_MPP_ERR))
     );
     assert_eq!(memory.read_u8(0x1000), Some(0));
     assert_eq!(memory.read_u16_be(0x1004), Some(0));
+}
+
+#[test]
+fn native_ppc_nbp_entity_packs_three_pascal_components() {
+    let mut memory = PpcSectionMem::new();
+    memory.add_region(0x1000, vec![0; 99]);
+    memory.add_region(0x1100, [3, b'O', b'b', b'j'].to_vec());
+    memory.add_region(0x1200, [4, b'T', b'y', b'p', b'e'].to_vec());
+    memory.add_region(0x1300, [5, b'Z', b'o', b'n', b'e', b'!'].to_vec());
+    let mut cpu = PpcCpu::new();
+    cpu.gpr[3] = 0x1000;
+    cpu.gpr[4] = 0x1100;
+    cpu.gpr[5] = 0x1200;
+    cpu.gpr[6] = 0x1300;
+
+    assert_eq!(
+        ppc_dispatch_appletalk_compatibility(
+            PpcAppleTalkCompatibilityOperation::NbpSetEntity,
+            &mut cpu,
+            &mut memory,
+        ),
+        PpcImportAction::ReturnPreserve,
+    );
+    assert_eq!(
+        ppc_memory_read_bytes(&mut memory, 0x1000, 4),
+        Some(vec![3, b'O', b'b', b'j']),
+    );
+    assert_eq!(
+        ppc_memory_read_bytes(&mut memory, 0x1000 + 33, 5),
+        Some(vec![4, b'T', b'y', b'p', b'e']),
+    );
+    assert_eq!(
+        ppc_memory_read_bytes(&mut memory, 0x1000 + 66, 6),
+        Some(vec![5, b'Z', b'o', b'n', b'e', b'!']),
+    );
 }
